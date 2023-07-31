4ebabb4d39b338274855f128fb80c2f9c6f97871
shenshi <shenshi@shouqianba.com>
2023-07-31 11:56:43 +0800

    chore: ci edit1

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 set -e
 
 
-push_addr='git@github.com/Ssshishi/Ssshishi.github.io.git' # git提交地址
-commit_info=`git describe --all --always --long`
+# push_addr='git@github.com/Ssshishi/Ssshishi.github.io.git' # git提交地址
+# commit_info=`git describe --all --always --long`
 dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
-push_branch=main # 推送的分支
+# push_branch=main # 推送的分支
 
 # 生成静态文件
 yarn build
@@ -15,10 +15,10 @@ yarn build
 # 进入生成的文件夹
 cd $dist_path
 
-git init
-git add -A
-git commit -m "deploy, $commit_info"
-git push -f $push_addr HEAD:$push_branch
+# git init
+# git add -A
+# git commit -m "deploy, $commit_info"
+# git push -f $push_addr HEAD:$push_branch
 
 cd -
 rm -rf $dist_path
